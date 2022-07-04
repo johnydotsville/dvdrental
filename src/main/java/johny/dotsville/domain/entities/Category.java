251351b2a9ef9fd1bbb0951b21201ad63f224011
johny dotsville <come.as@you.are>
2022-07-04 16:10:57 +0300

    Добавил поля в сущность Category

--- src/main/java/johny/dotsville/domain/entities/Category.java
+++ src/main/java/johny/dotsville/domain/entities/Category.java
@@ -1,4 +1,27 @@
 package johny.dotsville.domain.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "category")
+@Getter @Setter @NoArgsConstructor @ToString @EqualsAndHashCode
 public class Category extends AbstractEntity {
+    @Id
+    @Column(name = "category_id")
+    @SequenceGenerator(name = "category_id_gen", sequenceName = "category_category_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_id_gen")
+    private Long id;
+    @Column(name = "name")
+    private String name;
 }
