44597287c3eb2605600724a2f070a5ccc33f7982
johny dotsville <come.as@you.are>
2022-07-04 16:11:12 +0300

    Добавил поля в сущность Language

--- src/main/java/johny/dotsville/domain/entities/Language.java
+++ src/main/java/johny/dotsville/domain/entities/Language.java
@@ -1,4 +1,27 @@
 package johny.dotsville.domain.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "language")
+@Getter @Setter @NoArgsConstructor @ToString @EqualsAndHashCode
 public class Language extends AbstractEntity {
+    @Id
+    @Column(name = "language_id")
+    @SequenceGenerator(name = "language_id_gen", sequenceName = "language_language_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "language_id_gen")
+    private Long id;
+    @Column(name = "name")
+    private String name;
 }
