2ad76f14fc956365c0ad5d7c4a620f92eadbdd67
johny dotsville <come.as@you.are>
2022-07-04 16:25:58 +0300

    Заполнил сущность City полями

--- src/main/java/johny/dotsville/domain/entities/City.java
+++ src/main/java/johny/dotsville/domain/entities/City.java
@@ -1,4 +1,33 @@
 package johny.dotsville.domain.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "city")
+@Getter @Setter @NoArgsConstructor @ToString @EqualsAndHashCode
 public class City extends AbstractEntity {
+    @Id
+    @Column(name = "city_id")
+    @SequenceGenerator(name = "city_id_gen", sequenceName = "city_category_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "city_id_gen")
+    private Long id;
+    @Column(name = "city")
+    private String city;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
 }
