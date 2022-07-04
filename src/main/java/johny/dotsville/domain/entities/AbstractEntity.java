a1a918e93c20b3a6a94f3de7c7bea9e710a49d16
johny dotsville <come.as@you.are>
2022-07-04 12:09:53 +0300

    Добавил класс абстрактной сущности

---
+++ src/main/java/johny/dotsville/domain/entities/AbstractEntity.java
@@ -0,0 +1,21 @@
+package johny.dotsville.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter @ToString @EqualsAndHashCode
+public abstract class AbstractEntity {
+    @Column(name = "last_update", insertable = true, updatable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    @org.hibernate.annotations.Generated(
+            value = org.hibernate.annotations.GenerationTime.ALWAYS)
+    private LocalDateTime lastUpdate;
+}
