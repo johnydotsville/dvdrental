6624e153a2599c6d93b29f2794e8c1d1f3db9c11
johny dotsville <come.as@you.are>
2022-07-04 15:43:38 +0300

    Добавил в класс Actor мэппинг остальных полей

--- src/main/java/johny/dotsville/domain/entities/Actor.java
+++ src/main/java/johny/dotsville/domain/entities/Actor.java
@@ -1,4 +1,29 @@
 package johny.dotsville.domain.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "actor")
+@Getter @Setter @NoArgsConstructor @ToString @EqualsAndHashCode
 public class Actor extends AbstractEntity {
+    @Id
+    @Column(name = "actor_id")
+    @SequenceGenerator(name = "actor_id_gen", sequenceName = "actor_actor_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "actor_id_gen")
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
 }
