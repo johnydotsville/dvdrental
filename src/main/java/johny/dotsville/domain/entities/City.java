0a3181ccc28709cb04d1426ee167f2d5d3257453
johny dotsville <come.as@you.are>
2022-07-05 15:36:28 +0300

    Изменил тип связи

--- src/main/java/johny/dotsville/domain/entities/City.java
+++ src/main/java/johny/dotsville/domain/entities/City.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
@@ -28,7 +28,7 @@ public class City extends AbstractEntity {
     private Long id;
     @Column(name = "city")
     private String city;
-    @OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
     @JoinColumn(name = "country_id", nullable = false)
     private Country country;
 }
