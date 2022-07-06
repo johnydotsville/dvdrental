25cdee1c32216ba37bb705ea37602a88ea3c8201
johny dotsville <come.as@you.are>
2022-07-06 13:47:40 +0300

    Добавил сущность Customer

--- src/main/java/johny/dotsville/domain/entities/Customer.java
+++ src/main/java/johny/dotsville/domain/entities/Customer.java
@@ -1,4 +1,58 @@
 package johny.dotsville.domain.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customer")
+@Getter @Setter
 public class Customer extends AbstractEntity {
+    @Id
+    @Column(name = "customer_id")
+    @SequenceGenerator(name = "customer_id_gen", sequenceName = "customer_customer_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_gen")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @Column(name = "activebool")
+    private boolean activebool;
+
+    @Column(name = "last_update", insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @org.hibernate.annotations.Generated(
+            value = org.hibernate.annotations.GenerationTime.INSERT)
+    private LocalDateTime createAt;
+
+    @Column(name = "active")
+    private int active;
 }
