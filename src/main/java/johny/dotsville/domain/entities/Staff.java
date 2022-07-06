2b5d09f5c116cc29afe9030c5b3ae9888be2f5e5
johny dotsville <come.as@you.are>
2022-07-06 12:05:11 +0300

    Добавил сущности Staff и Store

--- src/main/java/johny/dotsville/domain/entities/Staff.java
+++ src/main/java/johny/dotsville/domain/entities/Staff.java
@@ -1,4 +1,54 @@
 package johny.dotsville.domain.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "staff")
+@Getter  @Setter
 public class Staff extends AbstractEntity {
+    @Id
+    @Column(name = "staff_id")
+    @SequenceGenerator(name = "staff_id_gen", sequenceName = "staff_staff_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "staff_id_gen", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    // TODO: Пока можно жить и без этого
+    private byte[] picture;
 }
--- src/main/java/johny/dotsville/domain/entities/Store.java
+++ src/main/java/johny/dotsville/domain/entities/Store.java
@@ -1,4 +1,33 @@
 package johny.dotsville.domain.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "store")
+@Getter @Setter
 public class Store extends AbstractEntity {
+    @Id
+    @Column(name = "store_id")
+    @SequenceGenerator(name = "store_id_gen", sequenceName = "store_store_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "store_id_gen", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "manager_staff_id")
+    private Staff manager;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id")
+    private Address address;
 }
