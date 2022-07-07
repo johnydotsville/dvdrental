d2eee4f9464cbc6d18ba13070c4873288d0c3778
johny dotsville <come.as@you.are>
2022-07-07 11:57:31 +0300

    Реализовал сущность Payment

--- src/main/java/johny/dotsville/domain/entities/Payment.java
+++ src/main/java/johny/dotsville/domain/entities/Payment.java
@@ -1,4 +1,47 @@
 package johny.dotsville.domain.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payment")
+@Getter @Setter
 public class Payment {
+    @Id
+    @Column(name = "payment_id")
+    @SequenceGenerator(name = "payment_id_gen", sequenceName = "payment_payment_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "payment_id_gen")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "staff_id")
+    private Staff staff;
+
+    // TODO: здесь в схеме БД в мануале почему-то связь М1, но в БД таких случаев нет
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rental_id")
+    private Rental rental;
+
+    @Column(name = "amount")
+    private double amount;
+
+    @Column(name = "payment_date")
+    private LocalDateTime paymentDate;
 }
